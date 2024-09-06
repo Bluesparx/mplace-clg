2108ca236c25e707bda3909799b20dfb45b41d55
nazia <naziahassan2288@gmail.com>
2024-09-06 09:40:33 +0530

    user.controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,58 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import {User} from "../models/user.model.js";
+import { uploadOnCloud } from "../utils/cloudinary.js";
+
 
 const registerUser = asyncHandler( async (req,res) => {
-    res.status(200).json({
-        message: "ok"
+    // get details from frontend - username, email, fullname, password
+    const {fullname, email, username, password} = req.body
+    // validation of these details - not empty
+    if(
+        [fullname, email, username, password].some((field) => field?.trim()==="")
+    ){
+        throw new ApiError(400, "All fields are required.")
+    }
+    // check if user already exists - email, username
+    const existingmail = User.findOne(email)
+    const existinguser = User.findOne(username)
+    if(existinguser){
+        throw new ApiError(409, "User with username already exists.")
+    }
+    if(existingmail){
+        throw new ApiError(409, "Email is already registered.")
+    }
+    // create user object - create entry in Db
+    const user = User.create({
+        username: username.toLowerCase(),
+        fullname,
+        email,
+        password,
     })
+    // remove password and refresh token from response
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+    // check for user creation
+    if(!createdUser){
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+    // return res
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered successfully")
+    )
+})
+
+const addItem = asyncHandler(async (req, res)=>{
+    const picLocalPath = req.files?.itempic[0].path;
+    if(!picLocalPath){
+        throw new ApiError(400, "Item picture is required.")
+    }
+    const itempic = await uploadOnCloud(picLocalPath)
+    if(!itempic){
+        throw new ApiError(400, "Picture is required.")
+    }
 })
 
 export {
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -21,7 +21,6 @@ const userSchema = new Schema ({
     fullname: {
         type: String,
         required: true,
-        unique: true,
         trim: true,
         index: true
     },
@@ -46,7 +45,7 @@ const userSchema = new Schema ({
 
 userSchema.pre("save", async function (next){
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
