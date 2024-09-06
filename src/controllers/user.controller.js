e99f50458d2563057844fdc670572c31434110fc
nazia <naziahassan2288@gmail.com>
2024-09-06 10:59:20 +0530

    user controls- register

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,39 +6,32 @@ import { uploadOnCloud } from "../utils/cloudinary.js";
 
 
 const registerUser = asyncHandler( async (req,res) => {
-    // get details from frontend - username, email, fullname, password
     const {fullname, email, username, password} = req.body
-    // validation of these details - not empty
     if(
         [fullname, email, username, password].some((field) => field?.trim()==="")
     ){
         throw new ApiError(400, "All fields are required.")
     }
-    // check if user already exists - email, username
-    const existingmail = User.findOne(email)
-    const existinguser = User.findOne(username)
+    const existingmail = await User.findOne(email)
+    const existinguser = await User.findOne(username)
     if(existinguser){
         throw new ApiError(409, "User with username already exists.")
     }
     if(existingmail){
         throw new ApiError(409, "Email is already registered.")
     }
-    // create user object - create entry in Db
-    const user = User.create({
-        username: username.toLowerCase(),
+    const user = await User.create({
+        username,
         fullname,
         email,
         password,
     })
-    // remove password and refresh token from response
     const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
     )
-    // check for user creation
     if(!createdUser){
         throw new ApiError(500, "Something went wrong while registering the user")
     }
-    // return res
     return res.status(201).json(
         new ApiResponse(200, createdUser, "User registered successfully")
     )
